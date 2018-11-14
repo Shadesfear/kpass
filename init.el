303a4463df96431a905b4e238419be2ad79c5030
Christopher Carman <chris@thecarmans.com>
2018-11-14 14:42:08 +0100

    backups

--- init.el
+++ init.el
@@ -230,3 +230,10 @@ ac-source-words-in-same-mode-buffers))
 
 (eval-after-load "org"
   '(require 'ox-md nil t))
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying      t  ; Don't de-link hard links
+      version-control        t  ; Use version numbers on backups
+      delete-old-versions    t  ; Automatically delete excess backups:
+      kept-new-versions      20 ; how many of the newest versions to keep
+      kept-old-versions      5) ; and how many of the old
