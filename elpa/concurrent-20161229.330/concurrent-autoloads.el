be3427beb1cb5a153588837f871a6867cc5f7f15
Christopher Carman <chris@thecarmans.com>
2018-11-14 16:01:25 +0100

    no

--- elpa/concurrent-20161229.330/concurrent-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; concurrent-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("concurrent.el") (23486 8212 187603 148000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; concurrent-autoloads.el ends here
